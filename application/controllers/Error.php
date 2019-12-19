8e6d358968ada8f8b07185a8016d1bb222f2c365
novinaldi <novinaldi1309@gmail.com>
2019-12-19 20:55:17 +0700

    Penambahan HMVC

---
+++ application/controllers/Error.php
@@ -0,0 +1,12 @@
+<?php
+class Error extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function index()
+    {
+        $this->load->view('error404.php');
+    }
+}
---
+++ application/modules/admin/controllers/Test.php
@@ -0,0 +1,12 @@
+<?php
+class Test extends CI_Controller
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function index()
+    {
+        echo 'test';
+    }
+}
